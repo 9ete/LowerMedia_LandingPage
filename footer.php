0971927da456f019c68e5d7e19532dc169bdb642
Pete Lower <pete.lower@gmail.com>
2013-09-03 17:42:40 -0500

    style adjustments, hide smiley

--- footer.php
+++ footer.php
@@ -10,17 +10,17 @@
  */
 ?>
 
-		</div><!-- #main -->
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<?php //get_sidebar( 'main' ); ?>
+	</div><!-- #main -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php //get_sidebar( 'main' ); ?>
 
-			<div class="site-info">
-				<?php //do_action( 'lowermedia_landingpage_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://kelseyhuebner.com/', 'kelseyhuebner' ) ); ?>" title="<?php esc_attr_e( 'Premium Web Construction & Application Development', 'lowemredia' ); ?>"><?php printf( __( 'Website designed and developed by %s', 'kelseyhuebner' ), 'Kelsey Huebner' ); ?></a>
-				
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
+		<div class="site-info">
+			<?php //do_action( 'lowermedia_landingpage_credits' ); ?>
+			<a href="<?php echo esc_url( __( 'http://kelseyhuebner.com/', 'kelseyhuebner' ) ); ?>" title="<?php esc_attr_e( 'Premium Web Construction & Application Development', 'lowemredia' ); ?>"><?php printf( __( 'Website designed and developed by %s', 'kelseyhuebner' ), 'Kelsey Huebner' ); ?></a>
+			
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
 
 	<?php wp_footer(); ?>
 </body>
